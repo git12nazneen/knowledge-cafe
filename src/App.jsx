8ee120090d6f22b7e29f90adf1d3465e91cd7100
git12nazneen <lipinazneen65@gmail.com>
2024-03-12 21:10:08 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <h1 className='text-5xl p-10 text-center bg-slate-900 text-white'>SPA knowledge cafe</h1>
+    </>
+  )
+}
+
+export default App
